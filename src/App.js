1335c19e25c7c1edff1abe967c9e4a38a2d50d9c
nenezoe <okerekenene7@gmail.com>
2022-06-07 09:53:47 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import UserFinder from './components/UserFinder';
+// import Users from './components/Users';
+
+function App() {
+  return (
+    <div>
+      <UserFinder />
+    </div>
+  );
+}
+
+export default App;
